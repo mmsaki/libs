42c1158cf5c5f63a3c2acc3c55f1b1e718132272
Meek Msaki <meek10x@gmail.com>
2026-01-26 08:56:16 -0600

    add base rpc client to jsonrpc lib

---
+++ packages/jsonrpc/src/base.ts
@@ -0,0 +1,45 @@
+import { JsonRpcClient } from "./client";
+
+export type RpcSpecBase = Record<string, RpcMethodSpec>;
+
+export abstract class BaseClient<MethodsSpec extends RpcSpecBase> {
+	rpc: JsonRpcClient<MethodsSpec>;
+	protected headers: Record<string, string> = {};
+
+	constructor(url: string) {
+		this.rpc = new JsonRpcClient(url);
+
+		return new Proxy(this, {
+			get: (target: this, prop: string | symbol, receiver) => {
+				// let real properties / methods through
+				if (prop in target) {
+					const value = Reflect.get(target, prop, receiver);
+					if (typeof value === "function") {
+						return (...args: any[]) => {
+							const result = value.apply(target, args);
+							// if method returns target, return proxy instead
+							return result === target ? receiver : result;
+						};
+					}
+					return value;
+				}
+				// dynamic rpc
+				if (typeof prop !== "string") return undefined;
+
+				const method = prop as keyof MethodsSpec;
+
+				return (
+					...params: MethodsSpec[typeof method]["params"]
+				): MethodsSpec[typeof method]["result"] =>
+					this.rpc.call(this.rpc.buildRequest(method, params), this.headers);
+			},
+		});
+	}
+
+	withHeaders(headers: Record<string, string>) {
+		this.headers = {
+			...headers,
+		};
+		return this;
+	}
+}
